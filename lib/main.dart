c82975a6a13ce6316c2b880239e989109b7bbe2f
Macbook <macbook@Macbooks-MacBook-Pro.local>
2021-01-05 14:49:00 +0600

    till 38 number video

--- lib/main.dart
+++ lib/main.dart
@@ -2,61 +2,73 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp(),);
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatefulWidget {
 
-  var questionIndex = 0;
+  @override
+  State<StatefulWidget> createState() {
+
+    return _MyAppState();
+  }
+
+}
+
+class _MyAppState extends State<MyApp>{
+
+  var _questionIndex = 0;
 
 
-  answerquestion(){
-      print("This answer chosen.");
-      questionIndex = questionIndex + 1;
-
-
+  answerQuestion(){
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+    print(_questionIndex);
   }
 
   @override
   Widget build(BuildContext context){
 
-    var questions = ['what\'s your favorite color?',
-      'what\'s your favorite animal?'];
+    var _questions = ['what\'s your favorite color?',
+                    'what\'s your favorite animal?'];
 
-     return MaterialApp(home:
-     Scaffold(
-       appBar: AppBar(
-         title: Text('App Bar'),
-       ),
-       body: Column(
-         children: [
-           Text(
-             //questions[0],
-               questions[questionIndex],
-           ),
-           
-           RaisedButton(
-             child: Text('Answer 1'),
-             onPressed: (answerquestion), // Here answerquestion is named function
-           ),
-           RaisedButton(
-             child: Text('Answer 2'),
-             onPressed: () {
-               print('Answer 2 chosen'); // It is Anonymous function.
-             },
-           ),
-           RaisedButton(
-             child: Text('Answer 3'),
-             onPressed: (){
-               print('Answer 3 chosen');
-             },
-           ),
-           RaisedButton(
-             child: Text('Answer 4'),
-             onPressed: (){
-               print('Answer 4 chosen');
-             },
-           ),
-         ],
-       ),
+    return MaterialApp(home:
+    Scaffold(
+      appBar: AppBar(
+        title: Text('App Bar'),
       ),
-     );
+      body: Column(
+        children: [
+          Text(
+            //questions[0],
+            _questions[_questionIndex],
+          ),
+
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: (answerQuestion), // Here answerQuestion is named function
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: () {
+              print('Answer 2 chosen'); // It is Anonymous function.
+            },
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: (){
+              print('Answer 3 chosen');
+            },
+          ),
+          RaisedButton(
+            child: Text('Answer 4'),
+            onPressed: (){
+              print('Answer 4 chosen');
+            },
+          ),
+        ],
+      ),
+    ),
+    );
   }
 }
+
+
